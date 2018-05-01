44fe65e6802758e99510d01bb39a21b888350dbb
GitDoi <nosnaut@gmail.com>
2018-05-01 14:30:55 -0700

    testing

--- src/Tests/AutoGravity.java
+++ src/Tests/AutoGravity.java
@@ -1,12 +1,32 @@
 package Tests;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import PageObjects.HomePage;
 
 public class AutoGravity extends BaseTest
 {
   @Test
-  public void reachCardApplication() 
+  public void reachCreditCardApplication() throws InterruptedException 
   {
-	 
+	  String zip = "92843";
+	  
+	  HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+	  
+	  //Choose to Shop New
+	  homePage.shopNewButton.click();
+	  
+	  //Enter zip code and submit.
+	  wait.until(ExpectedConditions.visibilityOf(homePage.locationPopUp));
+	  homePage.zipTextBox.sendKeys(zip);
+	  if (homePage.findButton.isEnabled())
+		  homePage.findButton.click();
+	  
+	  
+	  Thread.sleep(5);
   }
 }
+
+
--- src/Tests/BaseTest.java
+++ src/Tests/BaseTest.java
@@ -1,5 +1,7 @@
 package Tests;
 
+import java.util.Random;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,12 +12,15 @@ public class BaseTest
 {
 	public WebDriver driver;
 	public WebDriverWait wait;
+	public String baseURL = "Https://www.autogravity.com";
 	
 	@BeforeClass
 	public void setUp()
 	{
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		driver.get(baseURL);
+		wait = new WebDriverWait(driver, 5);
 	}
 	
 	@AfterClass
@@ -24,8 +29,10 @@ public class BaseTest
 		driver.quit();
 	}
 	
-	public void click()
+	public int generateRandomInteger(int min, int max)
 	{
-		
+		Random r = new Random();
+		r.setSeed(System.currentTimeMillis());
+		return r.nextInt((max - min) + 1) + min;
 	}
 }
